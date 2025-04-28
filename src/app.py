d22f4afe3f6cc2541676222b90b323a55bf6e26c
rebequinia <rebecadecamargo@gmail.com>
2025-04-28 20:21:02 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,43 +1,45 @@
 from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-data = {
-    "message": "Hello, World!"
-}
+    data = {"message": "Hello, World!"}
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    @app.route('/')
+    def home():
+        return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
 
-@app.route('/api/message', methods=['GET'])
-def get_message():
-    return jsonify(data)
+    @app.route('/api/message', methods=['GET'])
+    def get_message():
+        return jsonify(data)
 
-@app.route('/api/message', methods=['POST'])
-def create_message():
-    new_message = request.json.get('message')
-    if not new_message:
-        return jsonify({"error": "Missing 'message' field"}), 400
-    data['message'] = new_message
-    return jsonify({"message": "Message updated successfully", "new_message": data['message']}), 201
+    @app.route('/api/message', methods=['POST'])
+    def create_message():
+        new_message = request.json.get('message')
+        if not new_message:
+            return jsonify({"error": "Missing 'message' field"}), 400
+        data['message'] = new_message
+        return jsonify({"message": "Message created successfully", "new_message": data['message']}), 201
 
-@app.route('/api/message', methods=['PUT'])
-def update_message():
-    updated_message = request.json.get('message')
-    if not updated_message:
-        return jsonify({"error": "Missing 'message' field"}), 400
-    data['message'] = updated_message
-    return jsonify({"message": "Message updated successfully", "new_message": data['message']})
+    @app.route('/api/message', methods=['PUT'])
+    def update_message():
+        updated_message = request.json.get('message')
+        if not updated_message:
+            return jsonify({"error": "Missing 'message' field"}), 400
+        data['message'] = updated_message
+        return jsonify({"message": "Message updated successfully", "new_message": data['message']})
 
-@app.route('/api/message', methods=['DELETE'])
-def delete_message():
-    data['message'] = ''
-    return jsonify({"message": "Message deleted"}), 200
+    @app.route('/api/message', methods=['DELETE'])
+    def delete_message():
+        data['message'] = ''
+        return jsonify({"message": "Message deleted"}), 200
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', port=5000)
